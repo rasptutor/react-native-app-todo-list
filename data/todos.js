69565cf8dfc7f939a42601c61aaeedac7b3b6e0a
nelio <njafonso@gmail.com>
2024-11-19 15:14:04 +0200

    todo-list commit

---
+++ data/todos.js
@@ -0,0 +1,17 @@
+export const data = [
+    {
+        "id": 1,
+        "title": "Finished reading the book",
+        "completed": true
+    },
+    {
+        "id": 2,
+        "title": "Buy groceries",
+        "completed": false
+    },
+    {
+        "id": 3,
+        "title": "Write a blog post",
+        "completed": false
+    }
+]
